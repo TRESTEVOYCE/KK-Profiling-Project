a86a44bee0addf67d400f2567cf39f0c25e41149
mark <hitsugayasubaru@gmail.com>
2026-03-07 00:32:29 -0800

    created api views for users

--- API/views.py
+++ API/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from accounts.models import User
+from accounts.serializers import UserSerializer
+
+
 
 
 # Create your views here.
+
+class UserList(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)

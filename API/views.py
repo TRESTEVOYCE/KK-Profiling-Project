35ca42cfc4193786f059fe5ef5a14467c1459376
mark <hitsugayasubaru@gmail.com>
2026-03-07 01:34:11 -0800

    changed clasbased views to functional temporarily

--- API/views.py
+++ API/views.py
@@ -1,15 +1,43 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from accounts.models import User
+from rest_framework import response
 from accounts.serializers import UserSerializer
+from accounts.models import User
+from kkprofiles.serializers import ProfilingInformationsSerializer, KKAddressSerializer,KKAddressSerializer, YouthStatusSerializer
+from kkprofiles.models import ProfilingInformations,KKAddress,YouthStatus
+from events.serializers import EventSerializer
+from events.models import Event
+from rest_framework.decorators import api_view
 
 
+#used functions to return data in json format for now will be changed to class based views in the future
 
 
-# Create your views here.
+@api_view(['GET'])
+def UserList(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return response.Response(serializer.data)
 
-class UserList(APIView):
-    def get(self, request):
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
+@api_view(['GET'])
+def ProfilingInformationsList(request):     
+    profiling_informations = ProfilingInformations.objects.all()
+    serializer = ProfilingInformationsSerializer(profiling_informations, many=True)
+    return response.Response(serializer.data)
+    
+@api_view(['GET'])
+def KKAddressList(request):
+    kk_addresses = KKAddress.objects.all()
+    serializer = KKAddressSerializer(kk_addresses, many=True)
+    return response.Response(serializer.data)
+
+@api_view(['GET'])
+def YouthStatusList(request):
+    youth_statuses = YouthStatus.objects.all()
+    serializer = YouthStatusSerializer(youth_statuses, many=True)
+    return response.Response(serializer.data)
+
+@api_view(['GET'])
+def EventList(request):
+    events = Event.objects.all()
+    serializer = EventSerializer(events, many=True)
+    return response.Response(serializer.data)   
+

8d55c7426dad98a679dcfbb2c20590a579d7367b
mark <hitsugayasubaru@gmail.com>
2026-03-06 23:47:48 -0800

    created models for events

--- events/models.py
+++ events/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=255)
+    images = models.ImageField(upload_to='event_images/', blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name

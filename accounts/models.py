264d1cc1219581393f8ecda276f7a42b17aa6a4a
mark <hitsugayasubaru@gmail.com>
2026-03-07 00:15:28 -0800

    modified the models

--- accounts/models.py
+++ accounts/models.py
@@ -6,13 +6,9 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    username = models.CharField(max_length=150, unique=True)
+    username = models.CharField(max_length=150, unique=True,blank=True, null=True)
     password1 = models.CharField(max_length=128)
     password2 = models.CharField(max_length=128)
-
-
-    USERNAME_FIELD = 'email' 
-    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.email

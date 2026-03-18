2c8ac061c6c79116fe7260298716443cb9163bbc
mark <hitsugayasubaru@gmail.com>
2026-03-18 04:40:20 -0700

    changed api endpoints to cater classbased views

--- API/urls.py
+++ API/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from API import views
 
 urlpatterns = [
-    path('users/', views.UserList),
-    path('profiling_informations/', views.ProfilingInformationsList,name='profiling_informations_list'),
-    path('kk_addresses/', views.KKAddressList,name='kk_addresses_list'),
-    path('youth_statuses/', views.YouthStatusList,name='youth_statuses_list'),
-    path('events/', views.EventList,name='events_list'),
+    path('users/', views.UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', views.UserList.as_view(), name='user-detail'),
+    path('profiling-informations/', views.ProfilingInformationsList.as_view(), name='profiling-informations-list'),
+    path('profiling-informations/<int:pk>/', views.ProfilingInformationsList.as_view(), name='profiling-informations-detail'),
+    path('kk-addresses/', views.KKAddressList.as_view(), name='kk-address-list'),
+    path('kk-addresses/<int:pk>/', views.KKAddressList.as_view(), name='kk-address-detail'),
+    path('youth-statuses/', views.YouthStatusList.as_view(), name='youth-status-list'),
+    path('youth-statuses/<int:pk>/', views.YouthStatusList.as_view(), name='youth-status-detail'),
+    path('events/', views.EventList.as_view(), name='event-list'),
+    path('events/<int:pk>/', views.EventList.as_view(), name='event-detail'),
 ]

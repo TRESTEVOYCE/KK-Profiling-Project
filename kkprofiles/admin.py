474cf992024372d98f4f4949cb5fb46cee784228
mark <hitsugayasubaru@gmail.com>
2026-03-18 04:41:45 -0700

    registered kkaddress and youth status models to the admin for viewing purposes

--- kkprofiles/admin.py
+++ kkprofiles/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from kkprofiles.models import KKAddress, YouthStatus
 
 # Register your models here.
+admin.site.register(KKAddress)
+admin.site.register(YouthStatus) 

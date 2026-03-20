6f236e01a3e9ffb0d94834e964768868da84c108
mark <hitsugayasubaru@gmail.com>
2026-03-20 06:12:34 -0700

    added simple jwt for rest framewrok for authentication

--- config/settings.py
+++ config/settings.py
@@ -72,6 +72,13 @@ TEMPLATES = [
     },
 ]
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+
+}
 WSGI_APPLICATION = 'config.wsgi.application'
 
 

8544967a30ac0a632b122351c0c9e84993a997c1
mark <hitsugayasubaru@gmail.com>
2026-03-21 01:06:17 -0700

    fixed the default permissions and added it to settings so theirs no need for isAuthenticated

--- API/views.py
+++ API/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from accounts.models import User
 from accounts.serializers import UserSerializer
 from kkprofiles.models import ProfilingInformations, KKAddress, YouthStatus
@@ -7,31 +8,34 @@ from kkprofiles.serializers import (
     KKAddressSerializer,
     YouthStatusSerializer
 )
-
 from events.models import Event
 from events.serializers import EventSerializer
-
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    
 
 
 class ProfilingInformationsViewSet(viewsets.ModelViewSet):
     queryset = ProfilingInformations.objects.all()
     serializer_class = ProfilingInformationsSerializer
+    
 
 
 class KKAddressViewSet(viewsets.ModelViewSet):
     queryset = KKAddress.objects.all()
     serializer_class = KKAddressSerializer
+    
 
 
 class YouthStatusViewSet(viewsets.ModelViewSet):
     queryset = YouthStatus.objects.all()
     serializer_class = YouthStatusSerializer
+    
 
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+   
